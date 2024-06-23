4239c1b4487efd4cac11ea987778c5847979301f
Windree <windree@dragonsoul.com>
2024-06-23 11:43:15 +0000

    feat: move code into container

--- app.sh
+++ app.sh
@@ -1,54 +1,15 @@
 #!/bin/env bash
 set -Eeuo pipefail
 
-source "$(dirname "${BASH_SOURCE[0]}")/functions/is_contains.sh"
-
-declare -a args=
-success_phrase=
-
-while [ $# -gt 0 ]; do
-  case "$1" in
-  --success)
-    shift
-    success_phrase=$1
-    if [ $# -gt 0 ]; then
-      shift
-    fi
-    ;;
-  -s)
-    shift
-    subject=$1
-    if [ $# -gt 0 ]; then
-      shift
-    fi
-    ;;
-  *)
-    args+=($1)
-    shift
-    ;;
-  esac
-done
-
 dir=$(dirname "$(readlink -f -- "$0")")
 mutt_image=$(basename "$dir")-dar
+pipe=
+
 if [ -t 0 ]; then
   unset -v pipe
 else
   pipe=$(cat -)
 fi
-
-function main() {
-  build_image "$dir/image" "$mutt_image"
-  if [ -v pipe ]; then
-    local subj=$subject
-    if [ -n "$success_phrase" ]; then
-      subj=$(is_contains "$pipe" "$success_phrase" && echo "$subject ✓" || echo "$subject ⚠")
-    fi
-    echo "$pipe" | docker run --rm -i --env-file="$dir/.env" "$mutt_image" -s "$subj" "${args[@]}"
-  else
-    docker run --rm -it --env-file="$dir/.env" "$mutt_image" -s "$subject" "${args[@]}"
-  fi
-}
 
 function build_image() {
   if ! docker build --quiet "$1" -t "$2" 2>/dev/null >/dev/null; then
@@ -65,4 +26,11 @@ function validate() {
 }
 
 validate
-main
+
+build_image "$dir/image" "$mutt_image"
+
+if [ -v pipe ]; then
+  echo "$pipe" | docker run --rm -i --env-file="$dir/.env" "$mutt_image" "$@"
+else
+  docker run --rm -it --env-file="$dir/.env" "$mutt_image" "$@"
+fi
--- image/_/entrypoint.sh
+++ image/_/entrypoint.sh
@@ -1,13 +1,58 @@
 #!/bin/env bash
 set -Eeuo pipefail
 
+source "/functions/is_contains.sh"
+
 dir=$(dirname "$(readlink -f -- "$0")")
 config=/etc/msmtprc
+config_template=$config.template
+declare -a args=
+success_phrase=
+subject=
+
 if [ -t 1 ]; then
   unset pipe
 else
   pipe=$(cat -)
 fi
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+  --success)
+    shift
+    success_phrase=$1
+    if [ $# -gt 0 ]; then
+      shift
+    fi
+    ;;
+  -s)
+    shift
+    subject=$1
+    if [ $# -gt 0 ]; then
+      shift
+    fi
+    ;;
+  *)
+    args+=($1)
+    shift
+    ;;
+  esac
+done
+
+function main() {
+  cat "$config_template" >"$config"
+  create_config >>"$config"
+  local subj=$subject
+  if [ -n "$success_phrase" ]; then
+    subj=$(echo -n "$subject "; is_contains "$pipe" "$success_phrase" && echo "✓" || echo "⚠")
+  fi
+  if [ -v pipe ]; then
+    echo "$pipe" | mutt -s "$subj" "${args[@]}"
+  else
+    mutt -s "$subj" "${args[@]}"
+  fi
+
+}
 
 function create_config() {
   local error=0
@@ -79,13 +124,6 @@ function cleanup() {
   rm -f "$config"
 }
 
-trap cleanup exit
+trap cleanup EXIT HUP INT QUIT PIPE TERM
 
-cat "$config".template >"$config"
-create_config >>"$config"
-
-if [ -v pipe ]; then
-  echo "$pipe" | mutt "$@"
-else
-  mutt "$@"
-fi
+main
